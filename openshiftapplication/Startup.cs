4b74f4a08808982b0237700054e8733bc064fcd3
Daniel Villamizar <daniel8106@hotmail.com>
2023-01-24 14:52:30 -0500

    Update Startup.cs

--- openshiftapplication/Startup.cs
+++ openshiftapplication/Startup.cs
@@ -1,6 +1,6 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc;1
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
